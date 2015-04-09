341597eb27b0935927d46dcbef55b1509b535d5b
Mert Salih Kaplan <mail@mertskaplan.com>
2015-04-10 00:28:19 +0300

    Update

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div class="footer">
 	<div class="github">
 		<a class="license tooltip-left" data-tooltip="<?php echo _("Travian UAS is released under GNU General Public License, version 3."); ?>" href="/LICENSE">GPLv3</a>
-		<a target="_blank" href="https://github.com/mertskaplan/Travian-Unit-Advice-Simulator" title="GitHub" alt="GitHub"><img width="62" height="16" alt="GitHub" src="/images/github.svg" /></a>
+		<a target="_blank" href="https://github.com/mertskaplan/Travian-Unit-Advice-Simulator" alt="GitHub"><img width="62" height="16" alt="GitHub" src="/images/github.svg" /></a>
 	</div>
 </div>
 
