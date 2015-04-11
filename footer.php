794f49ffd2761d808b6d2c3bbae5d94f54127e39
Mert Salih Kaplan <mail@mertskaplan.com>
2015-04-11 21:04:42 +0300

    Update

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div class="footer">
 	<div class="github">
-		<a class="license tooltip-left" data-tooltip="<?php echo $locale_license ?>" href="/LICENSE">GPLv3</a>
+		<p class="code"><?php echo sprintf(_('coded with %2$s by %1$s'), $locale_mertskaplan, $locale_love); ?> | <a class="tooltip-left" data-tooltip="<?php echo $locale_license ?>" href="/LICENSE">GPLv3</a></p>
 		<a target="_blank" href="https://github.com/mertskaplan/Travian-Unit-Advice-Simulator" alt="GitHub"><img width="62" height="16" alt="GitHub" src="/images/github.svg" /></a>
 	</div>
 </div>
